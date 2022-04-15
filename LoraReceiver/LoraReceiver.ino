abfd30981f57b8d95fada75551a109105e5231de
Keyi <dingkeyi0121@outlook.com>
2022-04-14 21:48:46 -0400

    LoRa receiver receiving and parsing correctly

--- LoraReceiver/LoraReceiver.ino
+++ LoraReceiver/LoraReceiver.ino
@@ -2,7 +2,7 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-char buffer[10];
+char buffer[11];
 
 typedef struct{
     char header = 0x55;
@@ -10,7 +10,7 @@ typedef struct{
     char lat_dir;
     int32_t lon;
     char lon_dir;
-} Packet; 
+} Packet __attribute__((packed)); 
 
 void setup() {
   Serial.begin(9600);
@@ -27,21 +27,34 @@ void setup() {
 void loop() {
   // try to parse packet
   int packetSize = LoRa.parsePacket();
-  //packetSize must be 11
-  if (packetSize == 11 && LoRa.available()) {
+
+  //packet size is 11
+  if (packetSize==11) {
+    // received a packet
+    Serial.println("Received packet ");
+
     char first = (char)LoRa.read();
+    //the header is always 0x55
     if(first == 0x55) {
       buffer[0] = first;
       int i=1;
+      //read into buffer
       while(i<11) {
         buffer[i]=(char)LoRa.read();
         ++i;
       }
+      //parse the buffer into packet
       Packet * packet_ptr = (Packet *) buffer;
+      Serial.print("lat: ");
+      Serial.println(packet_ptr->lat);
+      Serial.print("lat_dir: ");
+      Serial.println(packet_ptr->lat_dir);
+      Serial.print("lon: ");
+      Serial.println(packet_ptr->lon);
+      Serial.print("lon_dir: ");
+      Serial.println(packet_ptr->lon_dir);
     }
-    
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
+
   }
+  
 }
